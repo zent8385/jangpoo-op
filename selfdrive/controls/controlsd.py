efa6aff644a6ea1a5ca419944aa3e3936cea1c02
atom <atom9123@gmail.com>
2020-03-31 21:31:44 +0900

    dd

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -317,8 +317,9 @@ def data_send(sm, pm, CS, CI, CP, VM, state, events, actuators, v_cruise_kph, rk
   CC.hudControl.lanesVisible = isEnabled(state)
   CC.hudControl.leadVisible = sm['plan'].hasLead
 
-  right_lane_visible = sm['pathPlan'].rProb > 0.1
-  left_lane_visible = sm['pathPlan'].lProb > 0.1
+  lane_visible = sm['pathPlan'].rProb
+  right_lane_visible = lane_visible > 0.2
+  left_lane_visible = lane_visible > 0.2
   CC.hudControl.rightLaneVisible = bool(right_lane_visible)
   CC.hudControl.leftLaneVisible = bool(left_lane_visible)
 
