ded5d9598c730d5b397a3c5980903c95f511a3de
atom <atom9123@gmail.com>
2020-04-09 08:33:13 +0900

    debug

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -433,8 +433,9 @@ class CarInterface(CarInterfaceBase):
 
 
   def log_update(self, can_string):
-      log_v_ego = ' v_ego={:5.0f} km/h'.format(self.CS.v_ego*CV.MS_TO_KPH)
-      if log_v_ego > 10:
+      v_ego = self.CS.v_ego * CV.MS_TO_KPH
+      if v_ego > 10:
+        log_v_ego = ' v_ego={:5.0f} km/h'.format( v_ego ) 
         log_data =  log_v_ego + str(self.CS.lkas11)
         self.traceLKA.add( log_data )
 
