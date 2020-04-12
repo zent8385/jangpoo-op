84db5d72ad105a79fd4ad1a33cb7c4086a64c7a0
atom <atom9123@gmail.com>
2020-04-12 15:56:03 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -204,7 +204,7 @@ class CarController():
           self.lkas_active_timer1 = 600
 
 
-    trace1.printf( 'A:{} Toq:{}  accel={} ns={} HDA={}'.format( steer_req, apply_steer, apply_accel,  new_steer, CS.Navi_HDA ) )
+    trace1.printf( 'A:{} Toq:{}  acc={} ns={} H={}'.format( steer_req, apply_steer, apply_accel,  new_steer, CS.Navi_HDA ) )
 
     self.apply_accel_last = apply_accel
     self.apply_steer_last = apply_steer
