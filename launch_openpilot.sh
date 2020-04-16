f0008bb3cf628d8824990813b63506a4478fd06c
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-17 03:48:21 +0900

    update

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -4,6 +4,9 @@ if [ ! -f /data/ota_updates ]; then
     /usr/bin/touch /data/no_ota_updates
 fi
 
+setprop persist.sys.locale $lang
+setprop persist.sys.local $lang
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
--- locale_change.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# Android system locale
-lang=ko-KR
-
-# change system locale
-setprop persist.sys.locale $lang
-setprop persist.sys.local $lang
