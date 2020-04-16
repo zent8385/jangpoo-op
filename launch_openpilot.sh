d5b8ec1c5d969c65494f174dde6c206b41214ee2
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-17 04:59:45 +0900

    update

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -4,17 +4,17 @@ if [ ! -f /data/ota_updates ]; then
     /usr/bin/touch /data/no_ota_updates
 fi
 
-if [ ! -f "/data/locale_updated" ]; then
-    update_locale=1
-else
-	update_locale=0
-fi
-
-if [ $update_locale -eq "1" ]; then
+if [ ! -f "/data/KRset" ]; then
+    sleep 3
+    mount -o remount,rw /system
+    cp -rf /data/openpilot/kyd/fonts.xml /system/etc/fonts.xml
+    chmod 644 /system/etc/fonts.xml
+    mount -o remount,r /system
+	
 	setprop persist.sys.locale ko-KR
 	setprop persist.sys.local ko-KR
 
-/usr/bin/touch /data/locale_updated
+	/usr/bin/touch /data/KRset
 fi
 
 export PASSIVE="0"
