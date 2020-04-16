e991326df078d7e1a5da749f1b8960412a22c0b1
multikyd <55026351+multikyd@users.noreply.github.com>
2020-04-17 04:13:35 +0900

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -4,31 +4,18 @@ if [ ! -f /data/ota_updates ]; then
     /usr/bin/touch /data/no_ota_updates
 fi
 
-if [ ! -f "/data/font_updated" ]; then
-    update_font=1
+if [ ! -f "/data/locale_updated" ]; then
+    update_locale=1
 else
-	update_font=0
+	update_locale=0
 fi
 
-if [ $update_font -eq "1" ]; then
-    # sleep 3 secs in case, make sure the /system is re-mountable
-    sleep 3
-    mount -o remount,rw /system
-		# install font
-        cp -rf /data/openpilot/selfdrive/assets/fonts/opensans* /system/fonts/
-        # change permissions
-        chmod 644 /system/etc/fonts.xml
-        chmod 644 /system/fonts/opensans*
+if [ $update_locale -eq "1" ]; then
+	setprop persist.sys.locale ko-KR
+	setprop persist.sys.local ko-KR
 
-    mount -o remount,r /system
-
-/usr/bin/touch /data/font_updated
-
+/usr/bin/touch /data/locale_updated
 fi
-
-# change system locale
-setprop persist.sys.locale ko-KR
-setprop persist.sys.local ko-KR
 
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
