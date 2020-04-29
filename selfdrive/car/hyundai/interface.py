a97ab36406354afd2c579c6570220c64958245b8
multikyd <multikyd@gmail.com>
2020-04-29 10:22:40 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -12,7 +12,7 @@ GearShifter = car.CarState.GearShifter
 ButtonType = car.CarState.ButtonEvent.Type
 
 class CarInterface(CarInterfaceBase):
-  def __init__(self, CP, CarController, CarState):
+  def __init__(self, CP, CarController, CarState, VehicleModel):
     self.CP = CP
     self.VM = VehicleModel(CP)
     self.frame = 0
