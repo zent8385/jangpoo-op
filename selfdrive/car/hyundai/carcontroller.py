3bb33ad3f8f81ee71b09ca6099f09bd1a3d50e45
multikyd <multikyd@gmail.com>
2020-04-29 12:28:07 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -128,10 +128,10 @@ class CarController():
     self.mdps12_cnt = frame % 0x100
 
     can_sends.append(create_lkas11(self.packer, self.car_fingerprint, 0, apply_steer, steer_req, self.lkas11_cnt, lkas_active,
-                                   CS.lkas11, lane_visible, left_lane_depart, right_lane_depart, keep_stock=True))
+                                   CS.lkas11, sys_warning, sys_state, left_lane_depart, right_lane_depart, keep_stock=True))
     if CS.mdps_bus or CS.scc_bus == 1: # send lkas12 bus 1 if mdps or scc is on bus 1
       can_sends.append(create_lkas11(self.packer, self.car_fingerprint, 1, apply_steer, steer_req, self.lkas11_cnt, lkas_active,
-                                   CS.lkas11, sys_warning, lane_visible, left_lane_depart, right_lane_depart, keep_stock=True))
+                                   CS.lkas11, sys_warning, sys_state, left_lane_depart, right_lane_depart, keep_stock=True))
     if CS.mdps_bus: # send clu11 to mdps if it is not on bus 0
       can_sends.append(create_clu11(self.packer, CS.mdps_bus, CS.clu11, Buttons.NONE, enabled_speed, self.clu11_cnt))
 
