fb27c0ba845980e1abf27bf5d1734f067aec3fc5
opbykyd <opbykyd@gmail.com>
2020-04-30 01:31:11 +0900

    update

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -123,7 +123,7 @@ def data_sample(CI, CC, sm, can_sock, state, mismatch_counter, can_error_counter
     else:
       events.append(create_event('calibrationInvalid', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
 
-  if CS.vEgo > 92 * CV.MPH_TO_MS:
+  if CS.vEgo > 150 * CV.KPH_TO_MS:
     events.append(create_event('speedTooHigh', [ET.NO_ENTRY, ET.SOFT_DISABLE]))
 
   # When the panda and controlsd do not agree on controls_allowed
