b3b102fb89f98f505af4d2fa8106bd6d5fbf960a
multikyd <multikyd@gmail.com>
2020-04-29 09:58:24 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -1,8 +1,8 @@
 from cereal import car
 from common.numpy_fast import clip
 from selfdrive.car import apply_std_steer_torque_limits
-from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, create_lfa_mfa \
-                                             create_scc12, create_mdps12
+from selfdrive.car.hyundai.hyundaican import create_lkas11, create_clu11, \
+                                             create_scc12, create_mdps12, create_lfa_mfa
 from selfdrive.car.hyundai.values import Buttons, SteerLimitParams, CAR
 from opendbc.can.packer import CANPacker
 
