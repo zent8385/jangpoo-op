a4cbc192fc993e628073bee3cab8e7d664a73d30
atom <atom9123@gmail.com>
2020-05-03 10:43:35 +0900

    dd

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -270,8 +270,8 @@ class CarController():
     vRel = int(vRel * 3.6 + 0.5)
 
     lead_objspd = CS.lead_objspd
-    str_log1 = 'torg:{:5.0f} limit={:5.0f} obj=[{:3.0f}/{:3.0f}][{:.0f}/{:.0f}]'.format( apply_steer, apply_steer_limit,  vRel, lead_objspd, dRel, CS.lead_distance  )
-    str_log2 = 'steer={:5.0f} sccInfo={:.0f} lkas={:.0f}'.format( CS.steer_torque_driver, CS.sccInfoDisp, CS.lkas_LdwsSysState )
+    str_log1 = 'torg:{:5.0f} obj=[{:3.0f}/{:2.0f}][{:2.0f}/{:3.0f}]'.format( apply_steer, vRel, lead_objspd, dRel, CS.lead_distance  )
+    str_log2 = 'steer={:5.0f} sccInfo={:2.0f} lkas={:.0f}'.format( CS.steer_torque_driver, CS.sccInfoDisp, CS.lkas_LdwsSysState )
     trace1.printf( '{} {}'.format( str_log1, str_log2 ) )
 
     self.apply_accel_last = apply_accel
