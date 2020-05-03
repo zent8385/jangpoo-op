d2bbf06fda162784d7233fbfa533b2a6aa514b61
atom <atom9123@gmail.com>
2020-05-03 09:54:38 +0900

    dd

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -972,7 +972,7 @@ static void ui_draw_debug(UIState *s)
   nvgText(s->vg, 0, 1020, speed_str, NULL);  
 
   snprintf(speed_str, sizeof(speed_str), "%s", scene->status.alert_text2 );
-  nvgText(s->vg, 0, 1020, speed_str, NULL);
+  nvgText(s->vg, 0, 1078, speed_str, NULL);
 
 }
 
