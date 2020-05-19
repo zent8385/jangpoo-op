66c1fd8f177770f23ad9eb14fb45e5f1212346f1
opbykyd <opbykyd@gmail.com>
2020-05-20 08:37:55 +0900

    Update manager.py

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -155,7 +155,7 @@ managed_processes = {
   "sensord": ("selfdrive/sensord", ["./sensord"]),
   "clocksd": ("selfdrive/clocksd", ["./clocksd"]),
   "gpsd": ("selfdrive/sensord", ["./gpsd"]),
-  "updated": "selfdrive.updated",
+  #"updated": "selfdrive.updated",
   "dmonitoringmodeld": ("selfdrive/modeld", ["./dmonitoringmodeld"]),
   "modeld": ("selfdrive/modeld", ["./modeld"]),
 }
