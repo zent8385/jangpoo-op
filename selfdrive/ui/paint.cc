0bab9f6ae78e39650124b7e9b43353a2cea880da
opbykyd <opbykyd@gmail.com>
2020-05-26 01:43:59 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -956,7 +956,7 @@ static void ui_draw_debug(UIState *s)
 
   snprintf(speed_str, sizeof(speed_str), "P:%8.5f", scene->pid.p );
   nvgText(s->vg, x_pos, y_pos+0, speed_str, NULL);
-  snprintf(speed_str, sizeof(speed_str), "I :%8.5f", scene->pid.i );
+  snprintf(speed_str, sizeof(speed_str), "I:%8.5f", scene->pid.i );
   nvgText(s->vg, x_pos, y_pos+50, speed_str, NULL);
   snprintf(speed_str, sizeof(speed_str), "F:%8.5f", scene->pid.f );
   nvgText(s->vg, x_pos, y_pos+100, speed_str, NULL);
