b60f1126c0eb05989b959c5a8cb24ed3b8479b53
opbykyd <opbykyd@gmail.com>
2020-06-02 02:01:28 +0900

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-if [ ! -f /data/no_ota_updates ]; then
+if [ ! -f "/data/no_ota_updates" ]; then
     /usr/bin/touch /data/no_ota_updates
 fi
 
-if [ -f /data/openpilot/op.py ]; then
+if [ -f "/data/openpilot/op.py" ]; then
     DIFF=$(/usr/bin/applets/diff /data/openpilot/op.py /system/comma/home/op.py)
     if [ "$DIFF" != "" ]; then
     sleep 3
@@ -19,7 +19,7 @@ if [ -f /data/openpilot/op.py ]; then
     fi
 fi
 
-if [ ! -f /system/comma/home/op.py ]; then
+if [ ! -f "/system/comma/home/op.py" ]; then
     sleep 3
     mount -o remount,rw /system
     cp -f /data/openpilot/op.sh /system/comma/home/
@@ -34,13 +34,13 @@ fi
 ALIAS_CHECK1=$(/usr/bin/grep tu /system/comma/home/.bash_profile)
 ALIAS_CHECK2=$(/usr/bin/grep op /system/comma/home/.bash_profile)
 
-if [ "$ALIAS_CHECK1" = "" ]; then
+if [ "$ALIAS_CHECK1" == "" ]; then
     sleep 3
     mount -o remount,rw /system
     echo "alias tu="/system/comma/home/tune.sh"" >> /system/comma/home/.bash_profile
     mount -o remount,r /system
 fi
-if [ "$ALIAS_CHECK2" = "" ]; then
+if [ "$ALIAS_CHECK2" == "" ]; then
     sleep 3
     mount -o remount,rw /system
     echo "alias op="/system/comma/home/op.sh"" >> /system/comma/home/.bash_profile
@@ -51,16 +51,17 @@ fi
 if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     sleep 3
     mount -o remount,rw /system
-	cp -rf /data/openpilot/selfdrive/assets/fonts/opensans* /system/fonts/
+  	cp -rf /data/openpilot/selfdrive/assets/fonts/opensans* /system/fonts/
     cp -rf /data/openpilot/kyd/fonts.xml /system/etc/fonts.xml
     chmod 644 /system/etc/fonts.xml
-	chmod 644 /system/fonts/opensans*
+  	chmod 644 /system/fonts/opensans*
     mount -o remount,r /system
 	
-	setprop persist.sys.locale ko-KR
-	setprop persist.sys.local ko-KR
-	setprop persist.sys.timezone Asia/Seoul
+	  setprop persist.sys.locale ko-KR
+  	setprop persist.sys.local ko-KR
+	  setprop persist.sys.timezone Asia/Seoul
 fi
 
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
+
