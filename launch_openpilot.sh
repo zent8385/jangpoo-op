0b973e024fcd73ebdf129f055bab70f675deb191
opbykyd <opbykyd@gmail.com>
2020-06-02 02:45:02 +0900

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -31,22 +31,22 @@ if [ ! -f "/system/comma/home/op.py" ]; then
     mount -o remount,r /system
 fi
 
-ALIAS_CHECK1=$(/usr/bin/grep tu /system/comma/home/.bash_profile)
-ALIAS_CHECK2=$(/usr/bin/grep op /system/comma/home/.bash_profile)
+#ALIAS_CHECK1=$(/usr/bin/grep tu /system/comma/home/.bash_profile)
+#ALIAS_CHECK2=$(/usr/bin/grep op /system/comma/home/.bash_profile)
 
-if [ "$ALIAS_CHECK1" == "" ]; then
-    sleep 3
-    mount -o remount,rw /system
-    echo "alias tu="/system/comma/home/tune.sh"" >> /system/comma/home/.bash_profile
-    mount -o remount,r /system
-fi
-if [ "$ALIAS_CHECK2" == "" ]; then
-    sleep 3
-    mount -o remount,rw /system
-    echo "alias op="/system/comma/home/op.sh"" >> /system/comma/home/.bash_profile
-    echo "/system/comma/home/op.sh" >> /system/comma/home/.bash_profile
-    mount -o remount,r /system
-fi
+#if [ "$ALIAS_CHECK1" == "" ]; then
+#    sleep 3
+#    mount -o remount,rw /system
+#    echo "alias tu="/system/comma/home/tune.sh"" >> /system/comma/home/.bash_profile
+#    mount -o remount,r /system
+#fi
+#if [ "$ALIAS_CHECK2" == "" ]; then
+#    sleep 3
+#    mount -o remount,rw /system
+#    echo "alias op="/system/comma/home/op.sh"" >> /system/comma/home/.bash_profile
+#    echo "/system/comma/home/op.sh" >> /system/comma/home/.bash_profile
+#    mount -o remount,r /system
+#fi
 
 if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     sleep 3
@@ -57,9 +57,9 @@ if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
   	chmod 644 /system/fonts/opensans*
     mount -o remount,r /system
 	
-	  setprop persist.sys.locale ko-KR
+	setprop persist.sys.locale ko-KR
   	setprop persist.sys.local ko-KR
-	  setprop persist.sys.timezone Asia/Seoul
+	setprop persist.sys.timezone Asia/Seoul
 fi
 
 export PASSIVE="0"
