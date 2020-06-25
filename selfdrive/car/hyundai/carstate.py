76741f78d35e0db332331b2e10e9f06d83f85500
janpoo6427 <janpoo6427@gmail.com>
2020-06-25 22:15:05 +0900

    setup cruise set speed

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -394,7 +394,9 @@ class CarState():
           elif delta_vsetdis > 5:
             cruise_set_speed_kph = self.VSetDis
           elif not self.curise_sw_check:
-            cruise_set_speed_kph += 1
+            #for tucson +2
+            #cruise_set_speed_kph += 1
+            cruise_set_speed_kph += 2
         elif self.prev_clu_CruiseSwState == 2:  # dn
           if self.curise_set_first:
             self.curise_set_first = 0
@@ -402,7 +404,9 @@ class CarState():
           elif delta_vsetdis > 5:
             cruise_set_speed_kph =  int(self.VSetDis)
           elif not self.curise_sw_check:
-            cruise_set_speed_kph -= 1
+            #for tucson -2
+            #cruise_set_speed_kph -= 1
+            cruise_set_speed_kph -= 2
 
         self.prev_clu_CruiseSwState = self.clu_CruiseSwState
       elif self.clu_CruiseSwState and delta_vsetdis > 0:
@@ -496,7 +500,7 @@ class CarState():
     #VSetDis
     if cp.vl['EMS16']['CRUISE_LAMP_M'] and not self.button_pressed:
       if cp.vl['CLU11']['CF_Clu_CruiseSwState'] == 2 and self.cruise_set_speed == 0 and self.clu_Vanz >= 30:
-        self.VSetDis = self.clu_Vanz * speed_conv
+        self.VSetDis = self.clu_Vanz  #* speed_conv
       elif self.VSetDis:
         if cp.vl['CLU11']['CF_Clu_CruiseSwState'] == 2:
           if self.prev_VSetDis:
@@ -515,6 +519,10 @@ class CarState():
       self.button_pressed = 0
 
 
+
+    if cp.vl["TCS13"]['DriverBraking'] or not cp.vl['EMS16']['CRUISE_LAMP_M'] or cp.vl['CLU11']['CF_Clu_CruiseSwState'] == 4:  
+      #self.cruise_set_speed_prev = self.cruise_set_speed
+      self.cruise_set_speed = 0
 
 
     self.is_set_speed_in_mph = int(cp.vl["CLU11"]["CF_Clu_SPEED_UNIT"])
@@ -656,6 +664,8 @@ class CarState():
        self.blinker_timer -= 1
     else:
        self.blinker_status = 0
+
+
 
 
 
