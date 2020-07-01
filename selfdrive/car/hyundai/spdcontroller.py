75af29a6d0e8da055bce2be1a9efaae46875d584
leejanghee <leejanghee@sindoh.com>
2020-07-02 08:29:51 +0900

    속도 셋 맞춰보자!!

--- selfdrive/car/hyundai/spdcontroller.py
+++ selfdrive/car/hyundai/spdcontroller.py
@@ -343,12 +343,6 @@ class SpdController():
             self.long_curv_timer += 1
 
 
-        set_speed_diff = set_speed - CS.clu_Vanz
-        if set_speed_diff > 2: #가속 필요
-            CS.VSetDis -= 2
-        elif set_speed_diff < -2: # 감속 필요
-            CS.VSetDis += 2
-
         # 선행 차량 거리유지
         lead_wait_cmd, lead_set_speed = self.update_lead( CS,  dRel, yRel, vRel)  
         # 커브 감속.
@@ -395,13 +389,11 @@ class SpdController():
                     btn_type = Buttons.SET_DECEL
         elif delta <= -1:
             set_speed = CS.VSetDis - dec_step_cmd
-            CS.VSetDis = set_speed
             self.seq_step_debug = 98   
             btn_type = Buttons.SET_DECEL
             self.long_curv_timer = 0
         elif delta >= 1 and (model_speed > 200 or CS.clu_Vanz < 70):
             set_speed = CS.VSetDis + dec_step_cmd
-            CS.VSetDis = set_speed
             self.seq_step_debug = 99
             btn_type = Buttons.RES_ACCEL
             self.long_curv_timer = 0            
@@ -409,12 +401,50 @@ class SpdController():
                 set_speed = CS.cruise_set_speed_kph
         
         
+        set_speed_diff = set_speed - CS.clu_Vanz
+        #ver2, ver3
+        CS.VSetDis = set_speed
+        #ver1
+        #CS.VSetDis = CS.clu_Vanz
+        #if set_speed_diff > 2: #가속 필요
+        #    CS.VSetDis -= 2
+        #elif set_speed_diff < -2: # 감속 필요
+        #    CS.VSetDis += 2
+
+
+        #ver2
+        # 고정 속도(2)만 가감
+        #CS.VSetDis = set_speed
+        if set_speed_diff > 1: #가속 필요
+            #크루즈 설정값은 set_speed 보다 낮아야 가속 신호를 보냄
+            CS.VSetDis -= 2
+        elif set_speed_diff < -1: # 감속 필요
+            #크루즈 설정값은 set_speed 보다 높아야 감속 신호를 보냄
+            CS.VSetDis += 2
+
+
+        #ver3
+        # 차이 속도 가감
+        #if set_speed_diff > 1: #가속 필요
+        #    CS.VSetDis -= set_speed_diff
+        #elif set_speed_diff < -1: # 감속 필요
+        #    CS.VSetDis += set_speed_diff
+        
+
+        
+        
+
+        
+        
+
+
 
         if CS.cruise_set_mode == 0:
             btn_type = Buttons.NONE
-
-        str3 = 'SS={:03.0f} TSS={:03.0f} VSD={:03.0f} DAt={:03.0f}/{:03.0f}/{:03.0f} DG/dec={:02.0f}/{:02.0f}'.format(
-            set_speed, target_set_speed, CS.VSetDis, CS.driverAcc_time, long_wait_cmd, self.long_curv_timer, self.seq_step_debug, dec_step_cmd )
+        #DAt={:03.0f}/{:03.0f}/{:03.0f} 
+        #CS.driverAcc_time, long_wait_cmd, self.long_curv_timer
+        str3 = 'SS={:03.0f} SSD={:03.0f} VSD={:03.0f} pVSD={:03.0f} DG/dec={:02.0f}/{:02.0f} '.format(
+            set_speed, set_speed_diff, CS.VSetDis, CS.prev_VSetDis, self.seq_step_debug, dec_step_cmd  )
         #str4 = ' LD/LS={:03.0f}/{:03.0f} '.format(  CS.lead_distance, CS.lead_objspd )
         str4 = ' LD/LS={:03.0f}/{:03.0f} '.format(  dRel, vRel )
 
