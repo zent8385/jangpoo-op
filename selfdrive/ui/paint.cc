779cbb3b11552ae5650464f116b338f6b9819a8f
neokii <neokii@gmail.com>
2020-07-26 01:10:30 +0900

    bug fix

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -577,7 +577,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
         val_color = nvgRGBA(255, 0, 0, 200);
       }
       // temp is alway in C * 10
-      snprintf(val_str, sizeof(val_str), "%d吏퇒", (int)(scene->maxCpuTemp/10));
+      snprintf(val_str, sizeof(val_str), "%d°", (int)(scene->maxCpuTemp/10));
       snprintf(uom_str, sizeof(uom_str), "");
     bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "CPU TEMP",
         bb_rx, bb_ry, bb_uom_dx,
@@ -598,7 +598,7 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
       val_color = nvgRGBA(255, 0, 0, 200);
     }
     // temp is alway in C * 1000
-    snprintf(val_str, sizeof(val_str), "%d吏퇒", (int)(scene->maxBatTemp/1000));
+    snprintf(val_str, sizeof(val_str), "%d°", (int)(scene->maxBatTemp/1000));
     snprintf(uom_str, sizeof(uom_str), "");
     bb_h +=bb_ui_draw_measure(s,  val_str, uom_str, "BAT TEMP",
         bb_rx, bb_ry, bb_uom_dx,
