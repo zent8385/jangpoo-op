c2814674238e3c0a319cef1c1d9fd7aca0bb7cf4
leejanghee <leejanghee@sindoh.com>
2020-08-11 03:37:28 +0900

    ..

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -799,6 +799,22 @@ static void bb_ui_draw_L_Extra(UIState *s)
     y += height;
     snprintf(str, sizeof(str), "modeSel: %.3d", scene->cruiseState.modeSel);
     ui_draw_text(s->vg, text_x, y, str, 25 * 2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
+
+    /*
+    nvgFontSize(s->vg, 80);
+    switch( scene.cruiseState.modeSel  )
+    {
+      case 0: strcpy( str_msg, "0.오파모드" ); nColor = COLOR_WHITE; break;
+      case 1: strcpy( str_msg, "1.커브모드" );    nColor = nvgRGBA(200, 200, 255, 255);  break;
+      case 2: strcpy( str_msg, "2.선행차" );  nColor = nvgRGBA(200, 255, 255, 255);  break;
+      case 3: strcpy( str_msg, "3.순정모드" );  nColor = nvgRGBA(200, 255, 255, 255);  break;
+      case 4: strcpy( str_msg, "4.오토모드" );  nColor = nvgRGBA(200, 255, 255, 255);  break;
+      default :  sprintf( str_msg, "%d", scene.cruiseState.modeSel ); nColor = COLOR_WHITE;  break;
+    }
+    nvgFillColor(s->vg, nColor);  
+    ui_print( s, x_pos, y_pos+80, str_msg );  
+    */
+
 }
 
 
