9cb509750e61e1e6b11f3d39f1c4870f3795597a
leejanghee <leejanghee@sindoh.com>
2020-08-12 15:56:29 +0900

    spd 테스트 2차

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -56,8 +56,8 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
 
 def create_clu11(packer, frame, bus, clu11, button, speed):
   values = clu11
-  #if button != 0:
-  values["CF_Clu_CruiseSwState"] = button
+  if button != 0:
+    values["CF_Clu_CruiseSwState"] = button
   values["CF_Clu_Vanz"] = speed
   values["CF_Clu_AliveCnt1"] = frame #frame % 0x10 
   return packer.make_can_msg("CLU11", bus, values)
