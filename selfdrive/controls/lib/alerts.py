842496f99f285e3e7db34977960796ee8738dfdb
leejanghee <leejanghee@sindoh.com>
2020-08-25 01:04:15 +0900

    활성 사운드 변경

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -78,14 +78,14 @@ ALERTS = [
       "",
       "",
       AlertStatus.normal, AlertSize.none,
-      Priority.MID, VisualAlert.none, AudibleAlert.chimeEngage, 2., 0., 0.),
+      Priority.MID, VisualAlert.none, AudibleAlert.chimeEngage, 1., 0., 0.),
 
   Alert(
       "disable",
       "",
       "",
       AlertStatus.normal, AlertSize.none,
-      Priority.MID, VisualAlert.none, AudibleAlert.chimeDisengage, 2., 0., 0.),
+      Priority.MID, VisualAlert.none, AudibleAlert.chimeDisengage, 1., 0., 0.),
 
   Alert(
       "fcw",
