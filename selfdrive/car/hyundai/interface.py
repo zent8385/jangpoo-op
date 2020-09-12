7dca4e26e54332cfd822c5987cc14f4b8feee966
janpoo6427 <janpoo6427@gmail.com>
2020-09-12 12:55:53 +0900

    pid 재적용

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -238,30 +238,30 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.0
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05], [0.01]]
-    # elif candidate == CAR.TUCSON_TL:
-    #   ret.lateralTuning.pid.kf = 0.00005
-    #   ret.mass = 1470. + STD_CARGO_KG
-    #   ret.wheelbase = 2.63
-    #   ret.steerRatio = 13.0
-    #   ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-    #   ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05], [0.01]]
     elif candidate == CAR.TUCSON_TL:
-      tire_stiffness_factor = 0.6
+      ret.lateralTuning.pid.kf = 0.00005
       ret.mass = 1985. + STD_CARGO_KG
       ret.wheelbase = 2.78
-      ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.ki = 0.01
-      ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
-      ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
-      ret.lateralTuning.lqr.c = [1., 0.]
-      ret.lateralTuning.lqr.k = [-100., 450.]
-      ret.lateralTuning.lqr.l = [0.22, 0.318]
-      ret.lateralTuning.lqr.dcGain = 0.003
-      ret.lateralTuning.lqr.scale = 2000
-      ret.steerRatio = 13.5 #14.4 * 1.1
-      ret.steerActuatorDelay = 0.1
-      ret.steerRateCost = 1.5
-      ret.steerLimitTimer = 0.8
+      ret.steerRatio = 13.0
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.05], [0.01]]
+    # elif candidate == CAR.TUCSON_TL:
+    #   tire_stiffness_factor = 0.6
+    #   ret.mass = 1985. + STD_CARGO_KG
+    #   ret.wheelbase = 2.78
+    #   ret.lateralTuning.init('lqr')
+    #   ret.lateralTuning.lqr.ki = 0.01
+    #   ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
+    #   ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
+    #   ret.lateralTuning.lqr.c = [1., 0.]
+    #   ret.lateralTuning.lqr.k = [-100., 450.]
+    #   ret.lateralTuning.lqr.l = [0.22, 0.318]
+    #   ret.lateralTuning.lqr.dcGain = 0.003
+    #   ret.lateralTuning.lqr.scale = 2000
+    #   ret.steerRatio = 13.5 #14.4 * 1.1
+    #   ret.steerActuatorDelay = 0.1
+    #   ret.steerRateCost = 1.5
+    #   ret.steerLimitTimer = 0.8
 
     ret.minEnableSpeed = -1.   # enable is done by stock ACC, so ignore this
 
--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -5,9 +5,9 @@ Ecu = car.CarParams.Ecu
 
 # Steer torque limits
 class SteerLimitParams:
-  STEER_MAX = 409 #255   # 409 is the max, 255 is stock
-  STEER_DELTA_UP = 3
-  STEER_DELTA_DOWN = 6
+  STEER_MAX = int(kegman.conf['steerMax']) #255   # 409 is the max, 255 is stock
+  STEER_DELTA_UP = int(kegman.conf['deltaUp'])
+  STEER_DELTA_DOWN = int(kegman.conf['deltaDown'])
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
