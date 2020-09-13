8c3f473e50dad847c68cfe407fd10a12bdc135db
janpoo6427 <62945300+janpoo6427@users.noreply.github.com>
2020-09-13 13:45:18 +0900

    ..

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -5,8 +5,8 @@ Ecu = car.CarParams.Ecu
 
 # Steer torque limits
 class SteerLimitParams:
-  kegman = kegman_conf()
-  STEER_MAX = int(kegman.conf['steerMax']) #255   # 409 is the max, 255 is stock
+  #kegman = kegman_conf()
+  STEER_MAX = 409 #int(kegman.conf['steerMax']) #255   # 409 is the max, 255 is stock
   STEER_DELTA_UP = int(kegman.conf['deltaUp'])
   STEER_DELTA_DOWN = int(kegman.conf['deltaDown'])
   STEER_DRIVER_ALLOWANCE = 50
