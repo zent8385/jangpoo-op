e3f1423effd03d45376305856ff2e4f0e05565be
janpoo6427 <janpoo6427@gmail.com>
2020-09-19 08:22:09 +0900

    ..

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -252,7 +252,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1730 + STD_CARGO_KG #1985. + STD_CARGO_KG
       ret.wheelbase = 2.67 #2.78
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1900.0
+      ret.lateralTuning.lqr.scale = 1850.0
       ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
@@ -262,7 +262,7 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.lqr.dcGain = 0.003
       ret.steerRatio = 13.82 #14.1 * 1.1 #13.7
       ret.steerActuatorDelay = 0.25
-      ret.steerRateCost = 0.8 #0.5
+      ret.steerRateCost = 0.9 #0.5
       ret.steerLimitTimer = 0.8
 
     ret.minEnableSpeed = -1.   # enable is done by stock ACC, so ignore this
