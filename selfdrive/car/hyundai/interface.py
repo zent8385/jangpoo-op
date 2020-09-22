87400b5d71b55d5c2398f17988b4ae9b4e21025f
janpoo6427 <62945300+janpoo6427@users.noreply.github.com>
2020-09-22 20:06:03 +0900

    ..

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -257,7 +257,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerLimitTimer = 0.8
       
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1850.0
+      ret.lateralTuning.lqr.scale = 1700.0
       ret.lateralTuning.lqr.ki = 0.01
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
